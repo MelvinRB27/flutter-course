ccbaeabc8110d6183f64e132b875a332c12e6291
Melvin Ruiz Batista <melvinruizbatista27.m@gmail.com>
2024-11-15 19:52:51 -0400

    aninated

--- lib/presentation/screen/animated/animated_screen.dart
+++ lib/presentation/screen/animated/animated_screen.dart
@@ -7,6 +7,28 @@ class AnimatedScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    bool selected = true;
+
+    return Scaffold(
+      appBar: AppBar(title: const Text("Animated container")),
+      body: Center(
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 400),
+          curve: Curves.easeInOut,
+          width: selected ? 200.0 : 100.0,
+          height: selected ? 100.0 : 200.0,
+          decoration: BoxDecoration(
+            color: selected ? Colors.red : Colors.blue,
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          selected = !selected;
+        },
+        child: const Icon(Icons.play_arrow_outlined),
+      ),
+    );
   }
 }
