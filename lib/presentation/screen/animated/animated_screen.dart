8e81daf2069bd0a30620d0b21fbec833176751c6
Melvin Ruiz Batista <melvinruizbatista27.m@gmail.com>
2024-11-16 11:34:15 -0400

    animated screen completado

--- lib/presentation/screen/animated/animated_screen.dart
+++ lib/presentation/screen/animated/animated_screen.dart
@@ -1,32 +1,51 @@
+import 'dart:math' show Random;
 import 'package:flutter/material.dart';
 
-class AnimatedScreen extends StatelessWidget {
+class AnimatedScreen extends StatefulWidget {
   static const String name = "Animated_Screen";
 
   const AnimatedScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    bool selected = true;
+  State<AnimatedScreen> createState() => _AnimatedScreenState();
+}
 
+class _AnimatedScreenState extends State<AnimatedScreen> {
+  double width = 200.0;
+  double height = 100.0;
+  Color color = Colors.red;
+  double borderRadius = 10.0;
+
+  void onChangeShape() {
+    final random = Random();
+    width = random.nextInt(300) + 50;
+    height = random.nextInt(300) + 50;
+    // color = Colors.primaries[random.nextInt(Colors.primaries.length)];
+    color = Color.fromRGBO(
+        random.nextInt(255), random.nextInt(255), random.nextInt(255), 1);
+    borderRadius = random.nextInt(300) + 50;
+
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Animated container")),
       body: Center(
         child: AnimatedContainer(
           duration: const Duration(milliseconds: 400),
           curve: Curves.easeInOut,
-          width: selected ? 200.0 : 100.0,
-          height: selected ? 100.0 : 200.0,
+          width: width,
+          height: height,
           decoration: BoxDecoration(
-            color: selected ? Colors.red : Colors.blue,
-            borderRadius: BorderRadius.circular(10),
+            color: color,
+            borderRadius: BorderRadius.circular(borderRadius),
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          selected = !selected;
-        },
+        onPressed: onChangeShape,
         child: const Icon(Icons.play_arrow_outlined),
       ),
     );
