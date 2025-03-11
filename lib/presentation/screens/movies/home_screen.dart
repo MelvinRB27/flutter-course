0c3920003d60f02fb3f922fef146be6f4da49971
Melvin Ruiz Batista <melvin@codika.net>
2025-03-11 10:33:19 -0400

    custom appbar

--- lib/presentation/screens/movies/home_screen.dart
+++ lib/presentation/screens/movies/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -30,13 +31,19 @@ class _HomeViewState extends ConsumerState<_HomeView> {
   @override
   Widget build(BuildContext context) {
     final nowPlayingMovies = ref.watch(nowPlayingMoviesProvider);
-    return ListView.builder(
-      itemCount: nowPlayingMovies.length,
-      itemBuilder: (context, index) {
-        return ListTile(
-          title: Text(nowPlayingMovies[index].title),
-        );
-      },
+    return Column(
+      children: [
+        const CustomAppbar(),
+        Expanded(
+            child: ListView.builder(
+          itemCount: nowPlayingMovies.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Text(nowPlayingMovies[index].title),
+            );
+          },
+        ))
+      ],
     );
   }
 }
